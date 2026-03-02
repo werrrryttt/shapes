468541a22635f1b6455c9ea3a35b92addda33dec
Алексей <alexeykarass5@gmail.com>
2026-03-02 16:06:24 +0300

    Revert "Add Matrix impl"

--- Source/Matrix.cpp
+++ Source/Matrix.cpp
@@ -1,32 +1,18 @@
-#include <stddef.h>
-#include <Matrix.h>
+#include "Matrix.h"
 
 Matrix::Matrix(size_t height, size_t width) : rows_(height), cols_(width)
 {
-    char *log;
-};
-
-
-class Matrix {
-    bool **array_;
-    size_t cols_;
-    size_t rows_;
-    const static IndexError indexError_ = IndexError{"Index out of range"};
-    void checkIndexes_(size_t row, size_t col) const
+    array_ = new bool* [height];
+    for (int i = 0; i < height; i++)
     {
-        if (row > rows_-1 || col > cols_-1)
-            throw indexError_;
+        array_[i] = new bool[width];
     }
 }
 Matrix::~Matrix()   
 {
     for (int i = 0; i < rows_; i++)
     {
-        array_ = new bool*[height];
-        for (int i = 0; i < height; i++)
-        {
-            array_[i] = new bool[width];
-        }
+        delete[] array_[i];
     }
     delete[] array_;
 }
